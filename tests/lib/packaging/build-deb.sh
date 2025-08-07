eb58db1b66fa080aaef43d15f0bc8445ec71986f
Katie May <katie.may@canonical.com>
2025-08-07 10:41:29 +0200

    a

--- tests/lib/packaging/build-deb.sh
+++ tests/lib/packaging/build-deb.sh
@@ -10,6 +10,7 @@ cd "$snapd_dir"
 go mod vendor
 su -c "cd $snapd_dir/c-vendor && ./vendor.sh" "$user"
 
-dch --newversion "$(cat "$vendor_tar_dir"/version)" "testing build"
+newver="$(dpkg-parsechangelog --show-field Version)"
+dch --newversion "1337.$newver" "testing build"
 unshare -n -- \
     su -l -c "cd $snapd_dir && DEB_BUILD_OPTIONS='nocheck testkeys ${buildopt}' dpkg-buildpackage -tc -b -Zgzip -uc -us" "$user"
