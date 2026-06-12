7901852aad2f8e35054eebeb1c9d6b4ea190401d
Katie May <katie.may@canonical.com>
2026-06-12 18:18:23 +0200

    patch with covermode automic

--- tests/utils/coverage/patch-builds.sh
+++ tests/utils/coverage/patch-builds.sh
@@ -4,6 +4,6 @@
 sed -i '/minidebuginfo/d' build-aux/snap/snapcraft.yaml
 
 # patch builds to include coverage
-sed -i '/go build/ { / -cover/! s/go build/go build -cover/ }' build-aux/snap/snapcraft.yaml
-sed -i '/^EXTRA_GO_BUILD_FLAGS = / { / -cover/! s/$/ -cover/ }' packaging/arch/PKGBUILD
-find packaging -type f -name snapd.spec -exec sed -i '/^EXTRA_GO_BUILD_FLAGS = / { / -cover/! s/$/ -cover/ }' {} \;
+sed -i '/go build/ { / -cover/! s/go build/go build -cover -covermode=atomic/ }' build-aux/snap/snapcraft.yaml
+sed -i '/^EXTRA_GO_BUILD_FLAGS = / { / -cover/! s/$/ -cover -covermode=atomic/ }' packaging/arch/PKGBUILD
+find packaging -type f -name snapd.spec -exec sed -i '/^EXTRA_GO_BUILD_FLAGS = / { / -cover/! s/$/ -cover -covermode=atomic/ }' {} \;
